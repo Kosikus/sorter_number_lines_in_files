c4ec70ff254fee32346c94b827dd60d122d5e369
Kos <ne.veshay.nos@gmail.com>
2020-05-17 03:26:31 +0300

    Init commit

---
+++ sort_problems_in_files.rb
@@ -0,0 +1,16 @@
+# получение относительных путей к .txt файлам в текущей папке
+file_paths = Dir[File.join(__dir__, '*.txt')]
+
+# запись содержимого каждого из файлов построчно в массив (массив массиво строк)
+files = file_paths.map { |file_path| File.readlines(file_path, encoding: 'UTF-8', chomp: true) }
+
+# удаление пустых строк в каждом из файлов
+files.each do |file|
+  file.delete_if { |line| line.chomp.empty? }
+end
+
+# сортировка строк по возрастанию (числовому) в каждом из файлов
+files = files.map { |file| file.sort { |a, b| a.to_i <=> b.to_i } }
+
+# запись текста из отсортированных строк в каждый из файлов
+file_paths.each_with_index { |file_path, index| File.write(file_path, files[index].join("\n")) }
