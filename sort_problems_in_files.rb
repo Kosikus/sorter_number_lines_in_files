58c328b64cdb6ad7eb58dfc1cafff9c2b5aacdb2
Kos <ne.veshay.nos@gmail.com>
2020-05-17 03:31:09 +0300

    Ending lines to each .txt file was added

--- sort_problems_in_files.rb
+++ sort_problems_in_files.rb
@@ -1,7 +1,7 @@
 # получение относительных путей к .txt файлам в текущей папке
 file_paths = Dir[File.join(__dir__, '*.txt')]
 
-# запись содержимого каждого из файлов построчно в массив (массив массиво строк)
+# запись содержимого каждого из файлов построчно в массив (массив массивов строк)
 files = file_paths.map { |file_path| File.readlines(file_path, encoding: 'UTF-8', chomp: true) }
 
 # удаление пустых строк в каждом из файлов
@@ -12,5 +12,5 @@ end
 # сортировка строк по возрастанию (числовому) в каждом из файлов
 files = files.map { |file| file.sort { |a, b| a.to_i <=> b.to_i } }
 
-# запись текста из отсортированных строк в каждый из файлов
-file_paths.each_with_index { |file_path, index| File.write(file_path, files[index].join("\n")) }
+# перезапись каждых из файлов файлами с отсортированными строками
+file_paths.each_with_index { |file_path, index| File.write(file_path, files[index].join("\n")) + "\n"}
